85155fa1f1354e39c6fa5013bab3004a180e0e16
princecena <cena.prince@gmail.com>
2017-07-04 21:26:06 +0530

    ok

--- understandingtypescriptcompiler/app.js
+++ understandingtypescriptcompiler/app.js
@@ -1,12 +1,10 @@
-namespace Understandingtypescriptcompiler {
-
-    let myName : string = "Max";
-    let myAge : number = 27;
-
+"use strict";
+var Understandingtypescriptcompiler;
+(function (Understandingtypescriptcompiler) {
+    var myName = "Max";
+    var myAge = 27;
     //myName = 30; //this assignment will give error but still compile as this is default typescript behaviour
     // We can override this behaviour by "noEmitOnError" : true in tsconfig.json
-
     //"sourceMap" : true in in tsconfig.json generates the js.map also which we can see in the Sources tab in the browser
     // and also get access to the ts file in browser and then place breakpoints for debugging
-
-}
+})(Understandingtypescriptcompiler || (Understandingtypescriptcompiler = {}));
--- usingtypesforabettercode/app.js
+++ usingtypesforabettercode/app.js
@@ -1,86 +1,100 @@
 "use strict";
-// string
-var myName = 'Max';
-// myName = 28;
-// number
-var myAge = 27;
-// myAge = 'Max';
-// boolean
-var hasHobbies = false;
-// hasHobbies = 1;
-// assign types
-var myRealAge;
-myRealAge = 27;
-// myRealAge = '27';
-// array
-var hobbies = ["Cooking", "Sports"];
-hobbies = [100];
-// hobbies = 100;
-// tuples
-var address = ["Superstreet", 99];
-// enum
-var Color;
-(function (Color) {
-    Color[Color["Gray"] = 0] = "Gray";
-    Color[Color["Green"] = 100] = "Green";
-    Color[Color["Blue"] = 2] = "Blue"; // 2
-})(Color || (Color = {}));
-var myColor = Color.Blue;
-console.log(myColor);
-// any
-var car = "BMW";
-console.log(car);
-car = { brand: "BMW", series: 3 };
-console.log(car);
-// functions
-function returnMyName() {
-    return myName;
-}
-console.log(returnMyName());
-// void
-function sayHello() {
-    console.log("Hello!");
-}
-// argument types
-function multiply(value1, value2) {
-    return value1 * value2;
-}
-// console.log(multiply(2, 'Max'));
-console.log(multiply(10, 2));
-// function types
-var myMultiply;
-// myMultiply = sayHello;
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(5, 2));
-// objects
-var userData = {
-    name: "Max",
-    age: 27
-};
-// userData = {
-//     a: "Hello",
-//     b: 22
-// };
-// complex object
-var complex = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
+var UsingTypeForBetterCode;
+(function (UsingTypeForBetterCode) {
+    // string
+    var myName = 'Max';
+    // myName = 28;
+    // number
+    var myAge = 27;
+    // myAge = 'Max';
+    // boolean
+    var hasHobbies = false;
+    // hasHobbies = 1;
+    // assign types
+    var myRealAge;
+    myRealAge = 27;
+    // myRealAge = '27';
+    // array
+    var hobbies = ["Cooking", "Sports"];
+    hobbies = [100];
+    // hobbies = 100;
+    // tuples
+    var address = ["Superstreet", 99];
+    // enum
+    var Color;
+    (function (Color) {
+        Color[Color["Gray"] = 0] = "Gray";
+        Color[Color["Green"] = 100] = "Green";
+        Color[Color["Blue"] = 2] = "Blue"; // 2
+    })(Color || (Color = {}));
+    var myColor = Color.Blue;
+    console.log(myColor);
+    // any
+    var car = "BMW";
+    console.log(car);
+    car = { brand: "BMW", series: 3 };
+    console.log(car);
+    // functions
+    function returnMyName() {
+        return myName;
     }
-};
-var complex2 = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
+    console.log(returnMyName());
+    // void
+    function sayHello() {
+        console.log("Hello!");
     }
-};
-// union types
-var myRealRealAge = 27;
-myRealRealAge = "27";
-// myRealRealAge = true;
-// check types
-var finalValue = 30;
-if (typeof finalValue == "number") {
-    console.log("Final value is a number");
-}
+    // argument types
+    function multiply(value1, value2) {
+        return value1 * value2;
+    }
+    // console.log(multiply(2, 'Max'));
+    console.log(multiply(10, 2));
+    // function types
+    var myMultiply;
+    // myMultiply = sayHello;
+    // myMultiply();
+    myMultiply = multiply;
+    console.log(myMultiply(5, 2));
+    // objects
+    var userData = {
+        name: "Max",
+        age: 27
+    };
+    // userData = {
+    //     a: "Hello",
+    //     b: 22
+    // };
+    // complex object
+    var complex = {
+        data: [100, 3.99, 10],
+        output: function (all) {
+            return this.data;
+        }
+    };
+    var complex2 = {
+        data: [100, 3.99, 10],
+        output: function (all) {
+            return this.data;
+        }
+    };
+    // union types
+    var myRealRealAge = 27;
+    myRealRealAge = "27";
+    // myRealRealAge = true;
+    // check types
+    var finalValue = 30;
+    if (typeof finalValue == "number") {
+        console.log("Final value is a number");
+    }
+    //never type - added with Typescript 2.0
+    function neverReturns() {
+        throw new Error('An Error!');
+    }
+    //Nullable types - added with Typescript 2.0 
+    var canBeNull = 12;
+    canBeNull = null; //if we set "strictNullChecks": true in tsconfig.json, it will give error that null cannot be assigned to number type
+    var canAlsoBeNull; //it's value is undefined as it is not initialized
+    canAlsoBeNull = null;
+    var canThisBeAny = null;
+    canThisBeAny = 12; //will give an error as we are assigning number to null type id we have set "strictNullChecks": true in tsconfig.json
+})(UsingTypeForBetterCode || (UsingTypeForBetterCode = {}));
